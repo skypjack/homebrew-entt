fffaf918db9e352c616f1ec094bb6824eaf2481b
Kerndog73 <kerndog73@gmail.com>
2018-05-31 17:34:13 +0930

    Add update.bash script
    This script takes the version as an argument (e.g. 2.6.0) and does everything else.
    
    This is what the script does
     0. Download the archive
     1. Compute the SHA256 hash
     2. Update the formula file with the URL and hash
     3. Run `brew audit --strict` to make sure the formula is OK
     4. Commit and push the new formula file

---
+++ update.bash
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# only argument should be the version to upgrade to
+if [ $# != 1 ]
+then
+  echo "Expected version to update to"
+  exit 1
+fi
+
+VERSION="$1"
+URL="https://github.com/skypjack/entt/archive/v$VERSION.tar.gz"
+FORMULA="entt.rb"
+
+# download the repo at the version
+curl "$URL" -L -s -o archive.tar.gz
+
+# compute sha256 hash
+HASH="$(openssl sha256 archive.tar.gz | cut -d " " -f 2)"
+
+# delete the archive
+rm archive.tar.gz
+
+# change the url in the formula file
+# the slashes in the URL must be escaped
+ESCAPED_URL="$(sed -e 's/[\/&]/\\&/g' <<< "$URL")"
+sed -i -e '/url/s/".*"/"'$ESCAPED_URL'"/' $FORMULA
+
+# change the hash in the formula file
+sed -i -e '/sha256/s/".*"/"'$HASH'"/' $FORMULA
+
+# sed creates a temporary file
+rm "$FORMULA-e"
+
+# run checks with homebrew
+# print error message and exit if the checks fail
+AUDIT_ERRORS="$(brew audit --strict $FORMULA)"
+if [ -n "$AUDIT_ERRORS" ]
+then
+  echo "$AUDIT_ERRORS"
+  exit 1
+fi
+
+# update remote repo
+git add entt.rb
+git commit -m "Update to v$VERSION"
+git push origin master
